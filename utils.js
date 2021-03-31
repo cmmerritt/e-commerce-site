8bf1e05abe3d80676383dc99b8bab9478cdecaaf
cmmerritt <christiane.merritt@gmail.com>
2021-03-31 15:53:49 -0700

    TDD dom render function

--- utils.js
+++ utils.js
@@ -41,8 +41,6 @@ export function findById(array, id) {
     for (let item of array) {
         if (item.id === id) {
             return item;
-        } else {
-            return null;
         }
     }
 }
